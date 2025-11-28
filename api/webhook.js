481f2f0570f69771c60c0814bac03d2782e15832
makbaze-sketch <makbaze@gmail.com>
2025-11-28 10:58:34 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,10 +1,12 @@
-import bot from "../bot.js";
+
+import { bot } from "../bot.js";
 
 export default async function handler(req, res) {
   try {
     await bot.handleUpdate(req.body);
-  } catch (err) {
-    console.error("Ошибка обработки webhook:", err);
+  } catch (e) {
+    console.error("Ошибка обработки webhook:", e);
   }
-  res.status(200).json({ ok: true });
+
+  return res.status(200).json({ ok: true });
 }
